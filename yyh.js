aa15a0947e46ff30ba511f1ec87fb9c25c252535
yinyehai <740673984@qq.com>
2016-05-04 17:37:05 +0800

    change
    change

--- yyh.js
+++ yyh.js
@@ -20,14 +20,14 @@
 	*@param 颜色
 	*/
 	var BlRect = (function(){
-		var BlRect = function(x, y, width,height,banjing,yanse)
+		var BlRect = function(x, y, width, height, banjing, background)
 		{
 			this.x = x;
 			this.y = y;
 			this.width = width;
 			this.height = height;
 			this.banjing = banjing;
-			this.yanse = yanse;
+			this.background = background;
 		};
 		BlRect.prototype.draw = function(ctx)
 		{
@@ -41,7 +41,7 @@
 			ctx.lineTo(this.width * 2, this.height);
 			ctx.lineTo(0, this.height);
 			ctx.closePath();
-			ctx.fillStyle = this.yanse;
+			ctx.fillStyle = this.background;
 			ctx.fill();
 
 			ctx.restore();
@@ -57,12 +57,13 @@
 	*@param 边框颜色
 	*/
 	var ZhezhaoYuan = (function(){
-		var ZhezhaoYuan = function(x, y, banjing, border)
+		var ZhezhaoYuan = function(x, y, banjing, borderSize, borderColor)
 		{
 			this.x = x;
 			this.y = y;
 			this.banjing = banjing;
-			this.border = border;
+			this.borderSize = borderSize;
+			this.borderColor = borderColor;
 		};
 		ZhezhaoYuan.prototype.draw = function(ctx)
 		{
@@ -73,34 +74,42 @@
 			ctx.arc(this.banjing, this.banjing, this.banjing, 0, 2*Math.PI);
 			ctx.fill();
 			ctx.restore();
-			if(this.border)
+			if(this.borderSize && this.borderColor)
 			{
-				ctx.strokeStyle = this.border;
+				ctx.lineWidth = this.borderSize;
+				ctx.strokeStyle = this.borderColor;
 				ctx.stroke();
 			}
 		}
 		return ZhezhaoYuan;
 	})();
 
-	/**文本对象
-	*@paran
+	/**数值类
+	*@param 数值
+	*@param 当前值
+	*@param x坐标
+	*@param y坐标
+	*@param 字体大小
+	*@param 字体
+	*@param 字体颜色
 	*/
 	var Shuzhi =  (function(){
-		var Shuzhi = function(shuzhi,cur,x,y,yanse){
+		var Shuzhi = function(shuzhi, cur, x, y, fontSize, fontFamily, fontColor){
 			this.shuzhi = shuzhi>1?shuzhi:shuzhi*100;
 			this.cur = cur;
 			this.x = x;
 			this.y = y;
-			this.font = '40px Arial';
-			this.yanse = yanse;
+			this.fontSize = fontSize;
+			this.fontFamily = fontFamily;
+			this.fontColor = fontColor;
 			this.textAlign = "center";
 			this.textBaseline = "middle";
 		};
 		Shuzhi.prototype.draw = function(ctx){
 			var str = this.cur + '%';;
 			ctx.save();
-			ctx.font = this.font;
-			ctx.fillStyle = this.yanse;
+			ctx.font = this.fontSize + ' ' + this.fontFamily;
+			ctx.fillStyle = this.fontColor;
 			ctx.translate(this.x, this.y);
 			ctx.textAlign = this.textAlign;
 			ctx.textBaseline = this.textBaseline;
@@ -112,7 +121,7 @@
 
 	//动画队列
 	var quee = [];
-	//创建动画
+	//动画方法
 	var aniamte = function(){
 		requestAnimFrame(aniamte);
 		for(var i=0; i<quee.length; i++)
@@ -121,33 +130,36 @@
 		}
 	};
 	aniamte.flage = true;
-	//动画方法
-	
 
 	//波浪球方法
 	var bolangqiu = function(opts){
 
-		var opts = opts || bolangqiu.opts;
-		var wrap = opts.wrap || bolangqiu.opts.wrap;
-		var banjing = opts.banjing || bolangqiu.opts.banjing;
-		var yanse = opts.yanse || bolangqiu.opts.yanse;
-		var border = opts.border || bolangqiu.opts.border;
-		var shuzhi = (typeof opts.shuzhi=='number')?opts.shuzhi:bolangqiu.opts.shuzhi;
-		var quanjing = banjing * 2;
+		var opts = opts || bolangqiu.opts,
+			wrap = opts.wrap || bolangqiu.opts.wrap,
+			banjing = opts.banjing || bolangqiu.opts.banjing,
+			background = opts.yanse || bolangqiu.opts.background,
+			fontSize = opts.fontSize || bolangqiu.opts.fontSize,
+			fontFamily = opts.fontFamily || bolangqiu.opts.fontFamily,
+			fontColor = opts.fontColor || bolangqiu.opts.fontColor,
+			borderSize = opts.borderSize || bolangqiu.opts.borderSize,
+			borderColor = opts.borderColor || bolangqiu.opts.borderColor,
+			shuzhi = (typeof opts.shuzhi=='number')?opts.shuzhi:bolangqiu.opts.shuzhi,
+			quanjing = banjing * 2;
 		
+		var offset = (borderSize - 1);
 		
 		//获取对象
 		var canvas = document.createElement('canvas');
-		canvas.width = canvas.height = banjing * 2;
+		canvas.width = canvas.height = quanjing + offset * 2;
 		if(wrap){
 			wrap.appendChild(canvas);
 		}else{
 			document.body.appendChild(canvas);
 		}
 		var ctx = canvas.getContext('2d');
-		var blRect = new BlRect(0, quanjing, quanjing, quanjing, 20,'#24cb24');
-		var zhezhao = new ZhezhaoYuan(0, 0, banjing, border);
-		var shuzhi = new Shuzhi(shuzhi, 0, banjing, banjing, "red");
+		var blRect = new BlRect(0 + offset, quanjing + offset, quanjing + offset, quanjing + offset, 20, background);
+		var zhezhao = new ZhezhaoYuan(0 + offset, 0 + offset, banjing, borderSize, borderColor);
+		var shuzhi = new Shuzhi(shuzhi, 0, banjing + offset, banjing + offset, fontSize, fontFamily, fontColor);
 		var speedX = banjing / 30;
 		
 		quee.push(function(){
@@ -163,7 +175,7 @@
 			}
 			
 
-			blRect.y =  quanjing - shuzhi.cur / 100 * quanjing;
+			blRect.y =  blRect.height - shuzhi.cur / 100 * blRect.height;
 
 			blRect.draw(ctx);
 			
@@ -188,11 +200,24 @@
 	
 	//默认值
 	bolangqiu.opts = {
+		//容器元素
 		wrap:null,
+		//球半径
 		banjing:100,
-		yanse:'#24cb24',
-		border:'#ccc',
-		shuzhi:50
+		//波浪颜色
+		background:'#24cb24',
+		//字体大小
+		fontSize:'40px',
+		//字体
+		fontFamily:'Arial',
+		//字体颜色
+		fontColor:'#f00',
+		//边框大小
+		borderSize:'1',
+		//边框颜色
+		borderColor:'#ccc',
+		//数值
+		shuzhi:100
 	};
 
 	global.YYH.bolangqiu = bolangqiu;
